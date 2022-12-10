8cc48c4a834d0914e49a0cde442e16c442e0c654
Guillaume Gouessan <guillaume.gouessan@gmail.com>
2022-12-10 12:19:23 +0100

    even simpler

--- 10/index.js
+++ 10/index.js
@@ -4,32 +4,27 @@ fs.readFile('./input.txt', (err, data) => {
 	if (err) throw err;
 
 	const input = data.toString(),
-		lines = input.split('\n').map((l) => {
-			const [c, n] = l.split(' ');
-			return [c, n && parseInt(n)];
-		});
+		lines = input.split('\n').map((l) => l.split(' '));
 
-	let r = 1;
-	let cycle = 0;
-	let firstStar = 0;
-	const pixels = [''];
+	let r = 1,
+		cycle = 0,
+		firstStar = 0,
+		secondStar = '';
+
 	const run = () => {
 		cycle++;
 		const p = cycle % 40;
-		pixels[pixels.length - 1] +=
-			(p || cycle) >= r && (p || cycle) <= r + 2 ? '#' : '.';
-		if (p === 0) pixels.push('');
-		if (p === 20) firstStar += r * cycle;
+		secondStar += (p || cycle) >= r && (p || cycle) <= r + 2 ? '#' : '.';
+		if (p === 0) secondStar += '\n';
+		else if (p === 20) firstStar += r * cycle;
 	};
 
 	for (const [cmd, n] of lines) {
 		run();
 		if (cmd === 'noop') continue;
 		run();
-		r += n;
+		r += parseInt(n);
 	}
-
-	const secondStar = pixels.join('\n');
 
 	console.log({ firstStar });
 	console.log(secondStar);
