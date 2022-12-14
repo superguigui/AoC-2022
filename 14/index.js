cfc0f4b69138fd9aef20ba51e4dfedbf0a0d15ad
Guillaume Gouessan <guillaume.gouessan@gmail.com>
2022-12-14 22:08:42 +0100

    day 14

---
+++ 14/index.js
@@ -0,0 +1,78 @@
+import fs from 'fs';
+
+fs.readFile('./input.txt', (err, data) => {
+	if (err) throw err;
+	const input = data.toString();
+
+	let abyss = 0;
+	const rocks = input.split('\n').map((l) =>
+		l.split(' -> ').map((c) => {
+			const p = c.split(',').map((n) => parseInt(n));
+			abyss = Math.max(p[1], abyss);
+			return p;
+		})
+	);
+	abyss += 2;
+	const origin = [500, 0];
+
+	const createGrid = () => {
+		const grid = new Map();
+		for (const rock of rocks) {
+			for (let i = 0; i < rock.length - 1; i++) {
+				const [x1, y1] = rock[i],
+					[x2, y2] = rock[i + 1];
+				if (y1 === y2) {
+					for (let j = Math.min(x1, x2); j <= Math.max(x1, x2); j++) {
+						if (!grid.has(y1)) grid.set(y1, new Set());
+						grid.get(y1).add(j);
+					}
+				} else if (x1 === x2) {
+					for (let j = Math.min(y1, y2); j <= Math.max(y1, y2); j++) {
+						if (!grid.has(j)) grid.set(j, new Set());
+						grid.get(j).add(x1);
+					}
+				}
+			}
+		}
+		return grid;
+	};
+
+	const fall = (grid, tester, exiter) => {
+		let [x, y] = [...origin]; // start
+		while (!exiter([x, y])) {
+			if (!tester([x, y + 1])) y++; // down
+			else if (!tester([x - 1, y + 1])) x--, y++; // down left
+			else if (!tester([x + 1, y + 1])) x++, y++; // down right
+			else {
+				if (!grid.has(y)) grid.set(y, new Set());
+				grid.get(y).add(x);
+				return true;
+			}
+		}
+		return false;
+	};
+
+	const solve = (tester, exiter) => {
+		const grid = createGrid();
+		let count = 0;
+		while (
+			fall(
+				grid,
+				(p) => tester(p, grid),
+				(p) => exiter(p, grid)
+			)
+		)
+			count++;
+		return count;
+	};
+
+	const firstStar = solve(
+		([x, y], grid) => grid.get(y)?.has(x),
+		([, y]) => y > abyss
+	);
+	const secondStar = solve(
+		([x, y], grid) => grid.get(y)?.has(x) || y >= abyss,
+		(_, grid) => grid.get(origin[1])?.has(origin[0])
+	);
+	console.log({ firstStar, secondStar });
+});
